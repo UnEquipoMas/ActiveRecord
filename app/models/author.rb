105670c1f0db1193555669d9e5828f3816fd9d68
Christian Camilo Vaca Daza <ccvacad@unal.edu.co>
2017-09-16 21:02:54 +0000

    Se agrega callback a autor

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,10 @@
 class Author < ApplicationRecord
     has_and_belongs_to_many :books
+    
+    before_destroy :delete_author
+
+    private
+        def delete_author
+            self.class.delete_all "author_id = #{id}"
+        end
 end
